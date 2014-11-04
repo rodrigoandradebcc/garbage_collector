f35e3980c1260ea3677cd35b371a54e4d8589bb2
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2014-11-04 10:31:23 -0300

    funcionalidade contatos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025214834) do
+ActiveRecord::Schema.define(version: 20141103172316) do
+
+  create_table "contacts", force: true do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
